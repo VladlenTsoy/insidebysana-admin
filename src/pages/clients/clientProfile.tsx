130098616b9e8f36d78c3d9685fa9a835e0a8dd6
Alexandr1807 <alexandrcyganov18@gmail.com>
2023-06-29 11:22:20 +0500

    fix code with source id & rewrite clients components

--- src/pages/clients/clientProfile.tsx
+++ src/pages/clients/clientProfile.tsx
@@ -1,23 +1,22 @@
 import React from 'react';
-import { Button, Descriptions } from 'antd';
-import { useLocation } from 'react-router-dom'
+import { Button} from 'antd';
+import { useParams } from 'react-router-dom'
 import { useGetClientQuery } from 'features/clients/clientsApi';
 import LoadingBlock from 'components/loading-block/LoadingBlock';
 import Container from 'layouts/container/Container';
-import { formatDate } from 'utils/formatDate';
 import HeaderPage from 'layouts/header-page/HeaderPage';
 import EditorClientsAction from 'features/clients/editor-clients-action/EditorClientsAction';
 import { EditOutlined, UserOutlined } from '@ant-design/icons';
+import ClientInfo from 'features/clients/client-info/clientInfo';
 
 const ClientProfile = () => {
-    const location = useLocation()
-    const id = location.state
-    const {data, isLoading} = useGetClientQuery(id)
+    const params = useParams<{id: string}>()
+    const {data, isLoading} = useGetClientQuery(params.id)
 
     if (isLoading) return <LoadingBlock title="Загрузка страницы..." />
 
     return (
-        <Container>
+        <>
             <HeaderPage
                 title="Информация о клиенте"
                 action={[
@@ -30,18 +29,10 @@ const ClientProfile = () => {
                 icon={<UserOutlined />}
                 tabs
             />
-            <Descriptions layout="vertical" bordered>
-                <Descriptions.Item label="Имя">{data.full_name}</Descriptions.Item>
-                <Descriptions.Item label="Email">{data.email}</Descriptions.Item>
-                <Descriptions.Item label="Телефон">{data.phone}</Descriptions.Item>
-                <Descriptions.Item label="Facebook">{data.facebook}</Descriptions.Item>
-                <Descriptions.Item label="Instagram">{data.instagram}</Descriptions.Item>
-                <Descriptions.Item label="Telegram">{data.telegram}</Descriptions.Item>
-                <Descriptions.Item label="Откуда">{data.source?.title}</Descriptions.Item>
-                <Descriptions.Item label="Дата рождения">{formatDate(data.date_of_birth)}</Descriptions.Item>
-                <Descriptions.Item label="Дата создания">{formatDate(data.created_at)}</Descriptions.Item>
-            </Descriptions>
-        </Container>
+            <Container>
+                <ClientInfo client={data}/>
+            </Container>
+        </>
     )
 }
 
